51379cc8a6e31a66d1eb0c7b2072a06de4d2e61b
thegi.code <thegi.code@gmail.com>
2021-01-30 22:31:17 +0900

    Add @loadable

---
+++ src/AppLoadable.js
@@ -0,0 +1,23 @@
+import React, { Suspense } from 'react';
+import loadable from '@loadable/component'
+
+// import OrderHook from './OrderHook';
+import './App.css';
+
+// const Home = loadable(() => import('./routes/Home'));
+const AsyncPage = loadable(props => import(`./routes/${props.page}`))
+
+
+function App() {
+
+  return (
+    <div className="App">
+      <div>
+	        {/* <Home /> */}
+          <AsyncPage page="Home" />
+      </div>
+    </div>
+  );
+}
+
+export default App;
